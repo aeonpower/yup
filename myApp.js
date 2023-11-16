87bda7fa4a99fb26431fea0ed9502002612d5dd8
aeon <150932695+aeonpower@users.noreply.github.com>
2023-11-16 18:03:15 +0200

    Updated removing x2 listen and port define

--- myApp.js
+++ myApp.js
@@ -2,65 +2,17 @@ const express = require('express');
 const helmet = require('helmet');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
 
-
-const PORT = process.env.PORT || 3030;
-
-app.listen(PORT, () => {
-  console.log(`bruh infosec server started on port ${PORT}`);
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
 app.use(express.static('public'));
 app.disable('strict-transport-security');
+const api = require('./server.js');
 app.use('/_api', api);
+
 app.get("/", function (request, response) {
   response.sendFile(__dirname + '/views/index.html');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+
+app.listen(PORT, () => {
+  console.log(`Your app is listening on port ${PORT}`);
 });
